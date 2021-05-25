27fa18dfbd13c6dd46242ead9719126de2507ec0
Gabriel <g.scozzarro@gmail.com>
2021-05-25 17:35:05 +0200

    fixing overfitting fixing plot creating report creating report pdf

--- Detecting_anomalies_electronic_wafer_production/detecting_anomalies_in_wafer_pruduction.R
+++ Detecting_anomalies_electronic_wafer_production/detecting_anomalies_in_wafer_pruduction.R
@@ -8,7 +8,6 @@
 
 # Tools ----
 library(tidyverse)
-library(visdat)
 library(ppsr)
 library(correlationfunnel)
 library(tidymodels)
@@ -62,12 +61,6 @@ data_model_ppsr_dummy$data<- head(data_model_ppsr_dummy$data, 20)
 data_model_ppsr_dummy + theme_ipsum() + labs(title = 'Features prediction power score', subtitle = 'Model algorithm: tree, metric: F1')
 
 
-nzv_pps_df<-data.frame(features = which(data_model_ppsr$data$x %in% colnames(train_df[,nzv_features])),
-                       pps = data_model_ppsr$data$pps[which(data_model_ppsr$data$x %in% colnames(train_df[,nzv_features]))])
-
-nzv_pps_df
-
-
 
 data_model_binned<- train_df %>% binarize()
 
@@ -75,7 +68,7 @@ cor_fun_plot<- data_model_binned %>% correlate(target = Class__1) %>%
                                      plot_correlation_funnel() +
                                      geom_point(size = 2, color = 'aquamarine3')
 
-cor_fun_plot$data<- head(cor_fun_plot$data, 30)
+cor_fun_plot$data<- head(cor_fun_plot$data, 40)
 
 cor_fun_plot
 
@@ -89,28 +82,35 @@ fit_xgb <- workflow() %>%
            add_recipe(recipe_spec) %>%
            fit(train_df)
 
-fit_xgb$fit$fit$fit %>% vip()
+vip_plot<- fit_xgb$fit$fit$fit %>% vip()
+
+vip_plot + theme_ipsum() + labs(title = 'Variable Importance Plots', subtitle = 'Importance extracted using XgBoost classification model')
+
+nzv_pps_df<-data.frame(features = which(data_model_ppsr$data$x %in% colnames(train_df[,nzv_features])),
+                       pps = data_model_ppsr$data$pps[which(data_model_ppsr$data$x %in% colnames(train_df[,nzv_features]))])
+
+
 
 # Feature Eng ----
 
 train_df<- train_df[,-nzv_features] #removing near zero o zero variance features
 
-train_df_smt <-  SMOTE(Class~., train_df, perc.over = 200)
+Validation_index<- createDataPartition(train_df$Class, p = 0.3, list = FALSE) #create validation set indx
+
+validation_df<- train_df[Validation_index,]
+train_df<- train_df[-Validation_index,]
+
+train_df_smt <-  SMOTE(Class~., train_df, perc.over = 200) #balancing df using SMOTE technique
 
 prop.table(table(train_df_smt$Class))
 
 #Model creation ----
 
-Validation_index<- createDataPartition(train_df_smt$Class, p = 0.3, list = FALSE)
-
-validation_df<- train_df_smt[Validation_index,]
-train_df_ready<- train_df_smt[-Validation_index,]
-
 library(h2o)
 
 h2o.init(nthreads = -1)
 
-train_h20<- as.h2o(train_df_ready)
+train_h20<- as.h2o(train_df_smt)
 validation_h20<- as.h2o(validation_df)
 
 x_h2o<- c(1:1522)
@@ -141,6 +141,8 @@ leader_perf
 leader_cf<- h2o.confusionMatrix(h2o_aml@leader)
 
 leader_cf
+
+plot(leader_perf, type = 'pr')
 
 plot(leader_perf, type = 'roc')
 
